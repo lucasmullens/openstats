64b7394c3bb31b4723c13f99018dafb67c742745
Lucas Mullens <lucasmullens@gmail.com>
2014-04-12 00:08:41 -0500

    post test

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var http = require('http');
 var path = require('path');
 
 var app = express();
+app.use(express.bodyParser());
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -29,7 +30,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/api', api.index);
-app.get('/api/track', api.track);
+app.post('/api/track', api.track);
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,6 @@ exports.index = function(req, res) {
     res.render('api', {});
 };
 exports.track = function(req, res) {
-	console.log(req.body.post);
+	console.log(req.body);
     res.send(200,{"status":"success"});
 };
